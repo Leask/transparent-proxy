0b97e98cabdd8b430d966a4f0d82b4d6d4cc997f
Leask Wong <i@leaskh.com>
2022-04-05 10:20:52 -0400

    working on https and wpad.dat

--- main.mjs
+++ main.mjs
@@ -4,10 +4,10 @@ const meta = await utilitas.which();
 const opts = { time: true };
 
 const socrates = new Socrates({
-    auth: (username, password) => {
-        utilitas.modLog(`Authenticate: ${username}:${password}.`, meta?.name, opts);
-        return username === 'leask' && password === 'nopassword';
-    },
+    // auth: (username, password) => {
+    //     utilitas.modLog(`Authenticate: ${username}:${password}.`, meta?.name, opts);
+    //     return username === 'leask' && password === 'nopassword';
+    // },
     // intercept: true,
 });
 
@@ -15,3 +15,17 @@ socrates.listen(8964, '', async () => {
     const { address, family, port } = socrates.address();
     utilitas.modLog(`Server started at ${address}${port} (${family}).`, meta?.title);
 });
+
+
+// import https from 'https';
+// import { DEFAULT_KEYS } from './lib/constants.mjs';
+
+// const options = {
+//     key: DEFAULT_KEYS.key,
+//     cert: DEFAULT_KEYS.cert,
+// };
+
+// https.createServer(options, function(req, res) {
+//     res.writeHead(200);
+//     res.end("hello world\n");
+// }).listen(8000);
